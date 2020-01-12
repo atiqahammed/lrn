fc6e790bbbcc2ddae84e201dc42ad7fdca2b5630
Atiq Ahammed <atiqahammedshamim@gmail.com>
2020-01-12 23:31:07 +0600

    recipe has been updated

--- lrn-ang-app/src/app/recipe/recipe-list/recipe-list.component.ts
+++ lrn-ang-app/src/app/recipe/recipe-list/recipe-list.component.ts
@@ -9,7 +9,8 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
 
   recipes: Recipe[] = [
-    new Recipe('Test Recipe', 'This is a test description for test recipe', 'https://www.bbcgoodfood.com/sites/default/files/styles/recipe/public/recipe/recipe-image/2019/08/meatball-black-bean-chilli.jpg?itok=n1KA3vhT')
+    new Recipe('Chaomin', 'Chow mein are Chinese noodles with vegetables and sometimes meat the name is a romanization of the Taishanese chāu-mèing. The dish is popular throughout the Chinese diaspora and appears on the menus of most Chinese restaurants abroad.', 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d1/Chow_mein_1_by_yuen.jpg/220px-Chow_mein_1_by_yuen.jpg'),
+    new Recipe('Samucha', 'A Samucha is a fried or baked pastry with a savory filling such as spiced potatoes, onions, peas, lentils, ground lamb, beef or chicken. The size, shape and consistency may vary, but many versions are triangular. Samuchas are often accompanied by chutney.', 'http://banglafreetips.com/wp-content/uploads/2018/04/testy-somocha.jpg')
   ];
 
   constructor() { }
