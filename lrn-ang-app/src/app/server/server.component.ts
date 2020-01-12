3f461c6cb869f6340e6cf8f6f676656277ac301a
Atiq Ahammed <Atiq@Nuarca.com>
2020-01-12 16:02:11 +0600

    data binding and event binding has been introduced

--- lrn-ang-app/src/app/server/server.component.ts
+++ lrn-ang-app/src/app/server/server.component.ts
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServerComponent implements OnInit {
 
-  constructor() { }
+  serverName = 'Test Server'
+  serverPID = 11;
+  serverStatus = 'offline'
+  serverStatusFlag = false;
+  toggleButtonDisabled = true;
+
+  constructor() { 
+    setTimeout(() => {
+      this.toggleButtonDisabled = false;
+    }, 5000);
+  }
 
   ngOnInit() {
+  }
+
+  toggoleServerStatus() {
+    this.serverStatusFlag = !this.serverStatusFlag;
+    if(this.serverStatusFlag) {
+      this.serverStatus = 'online';
+    } else {
+      this.serverStatus = 'offline';
+    }
   }
 
 }
