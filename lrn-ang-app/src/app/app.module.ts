ca4b275c6a42a6ee80a6b56a604e38b23c6c24e2
Atiq Ahammed <Atiq@Nuarca.com>
2020-01-12 18:53:13 +0600

    all component and items has been included

--- lrn-ang-app/src/app/app.module.ts
+++ lrn-ang-app/src/app/app.module.ts
@@ -7,11 +7,25 @@ import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms'
 import { from } from 'rxjs';
 import { ServerComponent } from './server/server.component';
+import { HeaderComponent } from './header/header.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { RecipeComponent } from './recipe/recipe.component';
+import { RecipeItemComponent } from './recipe/recipe-item/recipe-item.component';
+import { RecipeListComponent } from './recipe/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipe/recipe-detail/recipe-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent
+    ServerComponent,
+    HeaderComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent,
+    RecipeComponent,
+    RecipeItemComponent,
+    RecipeListComponent,
+    RecipeDetailComponent
   ],
   imports: [
     BrowserModule,
