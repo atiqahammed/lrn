363ec94c0cf96fe3a6fb9324c7fe888541400331
Atiq Ahammed <atiqahammedshamim@gmail.com>
2020-01-22 15:13:30 +0600

    Person extending EventEmitter

--- EventEmitter/app.js
+++ EventEmitter/app.js
@@ -8,3 +8,30 @@ eventEmitter.on('tutorial', (number1, number2) => {
 
 eventEmitter.emit('tutorial');
 eventEmitter.emit('tutorial', 2, 5);
+
+class Person extends EventEmitter {
+    
+    constructor(name) {
+        super();
+        console.log('Person created with name: ' + name);
+        this._name = name;
+    }
+
+    get name() {
+        return this._name;
+    }   
+}
+
+var atiq = new Person('Atiq');
+var shamim = new Person('Shamim');
+
+atiq.on('name', () => {
+    console.log('event for ' + atiq.name);
+});
+
+shamim.on('name', () => {
+    console.log('event for ' + shamim.name);
+});
+
+atiq.emit('name');
+shamim.emit('name');
