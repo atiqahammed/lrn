96b779e38c049e4ea93b61186dc5948ecdde23f7
Atiq Ahammed <atiqahammedshamim@gmail.com>
2020-01-22 13:57:57 +0600

    a module as a full object

--- RLtyhwFtXQA/app.js
+++ RLtyhwFtXQA/app.js
@@ -1,4 +1,11 @@
 console.log("hello world")
 
 const tutorial = require('./tutorial')
-console.log(tutorial(2, 4))
+
+console.log(tutorial)
+console.log(tutorial.sum)
+console.log(tutorial.sum(2, 4))
+console.log(tutorial.PI)
+console.log(new tutorial.SomeMathObject())
+
+//console.log(tutorial(2, 4))
