4ef663e11919476d3ff6b1b814423ea5338d357b
Atiq Ahammed <atiqahammedshamim@gmail.com>
2020-01-24 00:59:20 +0600

    component is working properly

---
+++ avater/src/Demo.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class Demo extends Component {
+    render() {
+        return <h2>Demo Component</h2>
+    }
+}
+
+export default Demo;
