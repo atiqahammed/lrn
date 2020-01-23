416e00f4078c96740102273c46a117f3f17a410a
Atiq Ahammed <atiqahammedshamim@gmail.com>
2020-01-24 01:06:41 +0600

    multiple line can be included in the render return using nested tag.

--- avater/src/Demo.js
+++ avater/src/Demo.js
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom';
 
 class Demo extends Component {
     render() {
-        return <h2>Demo Component</h2>
+        return <div>
+                    <h2>Demo Component</h2>
+                    <p>This is the demo component of my application</p>
+                </div>
     }
 }
 
