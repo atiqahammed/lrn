9d01b18cbca503b3f6c928a3857168758b9743d8
Atiq Ahammed <atiqahammedshamim@gmail.com>
2020-01-26 01:52:01 +0600

    data read from property variable (props)

--- avater/src/Avatar.js
+++ avater/src/Avatar.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './Avater.css';
+import 'tachyons'
 
-const Avater = () => {
-    return  <div className="avater_style">
+const Avater = (props) => {
+    return (
+            <div className="avater_style ma4 bg-light-purple dib pa4">
                 <img src="https://joeschmoe.io/api/v1/atiq"></img>
-                <h1>Atiq Ahammed</h1>
+                <h1>{props.name}</h1>
                 <p>I am a junior developer</p>
             </div>
+    )
 }
 
 export default Avater;
