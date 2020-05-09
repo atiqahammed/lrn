eb0e7221b7bda250416f1ab4b9376abf2a6ff500
Atiq Ahammed <atiqahammedshamim@gmail.com>
2020-05-09 10:02:25 -0400

    database server has been introduced

--- source/api-project/api/server.js
+++ source/api-project/api/server.js
@@ -2,11 +2,29 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const log4js = require('log4js');
-var logger = log4js.getLogger();
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+// Connection URL
+const url = 'mongodb://localhost:27017';
+// Database Name
+const dbName = 'myproject';
+// Create a new MongoClient
+const client = new MongoClient(url, { useUnifiedTopology: true });
 
+
+var logger = log4js.getLogger();
 logger.level = 'debug';
 logger.debug("API - Project starts...");
 
+
+client.connect(function(err) {
+    assert.equal(null, err);
+    logger.debug("Database Connected successfully to server");
+  
+    const db = client.db(dbName);
+  
+    client.close();
+});
 
 app.use(express.urlencoded());
 
