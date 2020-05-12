9bd013bdfa8dc9d5e38f6292a5e531e74e7b8ea0
Atiq Ahammed <atiqahammedshamim@gmail.com>
2020-05-12 14:22:16 +0600

    course informationg isbeing listed in a table view

--- react/flux-tutorial/my-app/src/components/CoursePage.js
+++ react/flux-tutorial/my-app/src/components/CoursePage.js
@@ -1,12 +1,51 @@
 import React from "react";
+import { getCourses } from "../api/courseApi";
 
-function CoursePage() {
-  return (
-    <div className="jumbotron">
-      <h1>Courses</h1>
-      <p>This page contains the list of course to have CRUD Operation.</p>
-    </div>
-  );
+class CoursePage extends React.Component {
+
+  state = {
+    courses: []
+  }
+
+  componentDidMount() {
+    getCourses().then((courses) => {
+      this.setState({courses: courses});
+    });
+  }
+
+  render() {
+    return (
+      <>
+        <div className="jumbotron">
+          <h1>Courses</h1>
+          <p>This page contains the list of course to have CRUD Operation.</p>
+        </div>
+        <div className="container">
+          <table className="table">
+            <thead>
+              <tr>
+                <th>Title</th>
+                <th>Author Id</th>
+                <th>Category</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.courses.map((courses) => {
+                return (
+                  <tr>
+                    <td>{courses.title}</td>
+                    <td>{courses.authorId}</td>
+                    <td>{courses.category}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      </>
+    );
+  }
+  
 }
 
 export default CoursePage;
--- react/flux-tutorial/my-app/src/components/HomePage.js
+++ react/flux-tutorial/my-app/src/components/HomePage.js
@@ -1,5 +1,4 @@
 import React from "react";
-import AboutPage from "./AboutPage";
 
 function HomePage() {
   return (
