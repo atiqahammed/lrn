05b24a3f46d6216653fab5e25ad3468bbfce7b9b
Atiq Ahammed <atiqahammedshamim@gmail.com>
2020-05-13 14:55:35 +0600

    redux implementation using previous template

--- react/redux-tutorial/redux-application/src/App.js
+++ react/redux-tutorial/redux-application/src/App.js
@@ -1,25 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import HomePage from './comonent/home/HomePage';
+import AboutPage from './comonent/about/AboutPage';
+import Header from './comonent/common/Header';
+import PageNotFound from './comonent/common/PageNotFound';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Switch>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/about" component={AboutPage} />
+        <Route component={PageNotFound} />
+      </Switch>
+    </>
   );
 }
 
