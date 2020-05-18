2b6c3ed591e3e0bc19e4792c1f9b6a60a967e1d8
Atiq Ahammed <atiqahammedshamim@gmail.com>
2020-05-18 10:10:24 -0400

    initial formate is ready

---
+++ react/author-quiz/src/AuthorQuiz.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function AuthorQuiz() {
+  return (
+    <h1>Hello world</h1>
+  );
+}
+
+export default AuthorQuiz;
--- react/author-quiz/src/index.js
+++ react/author-quiz/src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import AuthorQuiz from './AuthorQuiz';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AuthorQuiz />
   </React.StrictMode>,
   document.getElementById('root')
 );
