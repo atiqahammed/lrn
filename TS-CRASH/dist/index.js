d7c3e3d82852ae3360284ae0bcf9b2d054b128fa
Atiq Ahammed <atiqahammedshamim@gmail.com>
2021-08-30 22:02:10 +0600

    test html file has been checked for typescript compilation

--- TS-CRASH/dist/index.js
+++ TS-CRASH/dist/index.js
@@ -1,3 +1,4 @@
 "use strict";
 let id = 5;
+console.log(`${id} is the current id here..`);
 //id = '6'; //this will show error
--- TS-CRASH/src/index.ts
+++ TS-CRASH/src/index.ts
@@ -1,3 +1,5 @@
 let id: number = 5;
 
+console.log(`${id} is the current id here..`)
+
 //id = '6'; //this will show error
