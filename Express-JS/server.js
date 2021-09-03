827f890a781d7ede05debc2f7a3418fcb322f018
atiqahammed <atiqahammedshamim@gmail.com>
2021-09-03 19:54:03 +0600

    root path added for testing purpose

--- Express-JS/server.js
+++ Express-JS/server.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const app = express();
 
+
+app.get('/', (req, res) => {
+
+    console.log('here in the root path');
+    res.send('Hi there...');
+
+});
+
+
 app.listen(3000);
 
 
 
-console.log("test");
+//console.log("test");
